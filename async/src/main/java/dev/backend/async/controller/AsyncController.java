24db1460baad9f560d9013b86eb03568db68c3dd
newbe01 <pkwaa3@gmail.com>
2023-05-29 14:51:05 +0900

    async 를 사용하기위해서는 bean 주입을 반드시 받아야한다 AsyncService 의 case 2 , case 3 의 경우 spring bean 주입을 받지못해 비동기 적으로 처리가 되지 않는다

---
+++ async/src/main/java/dev/backend/async/controller/AsyncController.java
@@ -0,0 +1,32 @@
+package dev.backend.async.controller;
+
+import dev.backend.async.service.AsyncService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AsyncController {
+
+    private final AsyncService asyncService;
+
+    @GetMapping("/1")
+    public String asyncCall_1() {
+        asyncService.asyncCall_1();
+        return "success";
+    }
+
+    @GetMapping("/2")
+    public String asyncCall_2() {
+        asyncService.asyncCall_2();
+        return "success";
+    }
+
+    @GetMapping("/3")
+    public String asyncCall_3() {
+        asyncService.asyncCall_3();
+        return "success";
+    }
+
+}
