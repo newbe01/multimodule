a93e2ef9ecf44874c8715915710290ab58c06160
newbe01 <pkwaa3@gmail.com>
2023-05-29 16:46:33 +0900

    no message

--- feign/src/main/java/dev/backend/feign/feign/logger/FeignCustomLogger.java
+++ feign/src/main/java/dev/backend/feign/feign/logger/FeignCustomLogger.java
@@ -24,8 +24,20 @@ public class FeignCustomLogger  extends Logger {
 
     @Override
     protected void log(String configKey, String format, Object... args) {
-
-        //  log 를 어떤 형식으로 남길지 정의
+        /**
+         * [값]
+         * configKey = DemoFeignClient#callGet(String,String,Long)
+         * logLevel = BASIC # "feign.client.config.demo-client.loggerLevel" 참고
+         *
+         * [동작 순서]
+         * `logRequest` 메소드 진입 -> 외부 요청 -> `logAndRebufferResponse` 메소드 진입
+         *
+         * [참고]
+         * request에 대한 정보는
+         * `logAndRebufferResponse` 메소드 파라미터인 response에도 있다.
+         * 그러므로 request에 대한 정보를 [logRequest, logAndRebufferResponse] 중 어디에서 남길지 정하면 된다.
+         * 만약 `logAndRebufferResponse`에서 남긴다면 `logRequest`는 삭제해버리자.
+         */
         System.out.println(String.format(methodTag(configKey) + format , args));
 
     }
